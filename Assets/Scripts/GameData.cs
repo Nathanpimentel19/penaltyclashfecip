3854a8497b0142bfec6af596cbbc766c87e44315
jordao000 <arthurjordaocorrea@gmail.com>
2026-08-31 20:55:05 -0300

    coisas do placar

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -4,4 +4,10 @@ public static class GameData
 {
     public static int PontuacaoAtual = 0;
     public static string PaisSelecionado = "";
+
+    public static string SiglaTime1 = "BRA";
+    public static Sprite BandeiraTime1;
+
+    public static string SiglaTime2 = "FRA";
+    public static Sprite BandeiraTime2;
 }
